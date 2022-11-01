be53debd00aaca848c68e91427933e9f3eb9cc26
Julien Boriasse <julien@boriasse.com>
2022-11-01 14:44:46 -0400

    Initial code bsed on esp32-tcp-client

---
+++ include/secrets.h
@@ -0,0 +1,2 @@
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "mot_de_passe"
---
+++ src/main.cpp
@@ -0,0 +1,89 @@
+// Source : https://www.dfrobot.com/blog-948.html
+
+#include <Arduino.h>
+#include "secrets.h"
+#include "WiFi.h"
+
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
+
+const char *host = "206.167.45.110";
+const int port = 10000;
+
+String translateEncryptionType(wifi_auth_mode_t encryptionType)
+{
+
+  switch (encryptionType)
+  {
+  case (WIFI_AUTH_OPEN):
+    return "Open";
+  case (WIFI_AUTH_WEP):
+    return "WEP";
+  case (WIFI_AUTH_WPA_PSK):
+    return "WPA_PSK";
+  case (WIFI_AUTH_WPA2_PSK):
+    return "WPA2_PSK";
+  case (WIFI_AUTH_WPA_WPA2_PSK):
+    return "WPA_WPA2_PSK";
+  case (WIFI_AUTH_WPA2_ENTERPRISE):
+    return "WPA2_ENTERPRISE";
+  }
+}
+
+void scanNetworks()
+{
+
+  int numberOfNetworks = WiFi.scanNetworks();
+
+  Serial.print("Number of networks found: ");
+  Serial.println(numberOfNetworks);
+
+  for (int i = 0; i < numberOfNetworks; i++)
+  {
+
+    Serial.print("Network name: ");
+    Serial.println(WiFi.SSID(i));
+
+    Serial.print("Signal strength: ");
+    Serial.println(WiFi.RSSI(i));
+
+    Serial.print("MAC address: ");
+    Serial.println(WiFi.BSSIDstr(i));
+
+    Serial.print("Encryption type: ");
+    String encryptionTypeDescription = translateEncryptionType(WiFi.encryptionType(i));
+    Serial.println(encryptionTypeDescription);
+    Serial.println("-----------------------");
+  }
+}
+
+void connectToNetwork()
+{
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Establishing connection to WiFi..");
+  }
+
+  Serial.println("Connected to network");
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  scanNetworks();
+  connectToNetwork();
+
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  
+  delay(2000);
+}
